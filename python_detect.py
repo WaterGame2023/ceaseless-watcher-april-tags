d82b893ff8f6b262f01cac9657384e57d727bcc1
WaterGame2023 <spam4alaric@gmail.com>
2022-10-27 19:59:48 -0500

    I was dumb when changing things

--- python_detect.py
+++ python_detect.py
@@ -167,9 +167,9 @@ while looping:
             # roll  = eulerAngles[2] #Debug
 
             #Output yaw, pitch, roll values to command line
-            print("\nYaw", yaw)
-            print("pitch", pitch)
-            print("roll", roll)
+            print("\nYaw", yaw_deg)
+            print("pitch", pitch_deg)
+            print("roll", roll_deg)
 
             #Output yaw, pitch, roll values in degrees to NetworkTables
             NT.putString("yaw_deg", yaw_deg)   #Yaw
